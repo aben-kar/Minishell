c8b7967c1eb159de751a765c4992cf0118f26787
achraf <achrafbenkardoud@gmail.com>
2025-05-14 21:08:52 +0100

    shell-1

--- main.c
+++ main.c
@@ -3,95 +3,6 @@
 // define global variable
 int g_exit_status = 0;
 
-// int main(int ac, char **av, char **envp)
-// {
-// 	(void)av;
-// 	// (void)envp;
-// 	if (ac == 1)
-// 	{
-// 		char *line;
-// 		t_token *tokens;
-// 		t_command *cmds = NULL;
-// 		int has_pipe;
-// 		t_gc *gc;
-// 		t_env *env_list = init_copier_env(envp);
-// 		t_env *env = ft_copier_env(env_list, envp);
-
-// 		while (1)
-// 		{
-// 			setup_signals();
-// 			write(1, "minishell$ ", 11);
-// 			line = get_next_line(0); // no readline, works with ctrl-D too
-// 			if (!line)
-// 			{
-// 				write(1, "exit\n", 5);
-// 				break;
-// 			}
-// 			if (line[0] == '\n')
-// 			{
-// 				free(line);
-// 				continue;
-// 			}
-
-// 			gc = NULL; // reset GC for each loop
-
-// 			tokens = tokenize(line, &gc);				 // assumes tokenize may call gc_alloc()
-// 			cmds = parse_tokens(tokens, &has_pipe, &gc); // same here
-// 														 // Inside your while(1) loop, after parse_tokens():
-// 			t_command *current = cmds;
-
-// 			if (current)
-// 			{
-// 				printf ("hena\n");
-// 				execute_command(current, env);
-// 			}
-// 			// while (current)
-// 			// {
-// 			// 	printf("Command: %s\n", current->cmd[0]); // Print first arg (e.g., "ls")
-// 			// 	printf("Args: ");
-// 			// 	for (int i = 0; current->cmd[i]; i++)
-// 			// 		printf("[%s] ", current->cmd[i]);
-// 			// 	printf("\n");
-
-// 			// 	// printf("Has pipe? %d\n", current->has_pipe); // Or equivalent field
-// 			// 	// printf("In FD: %d, Out FD: %d\n", current->in_fd, current->out_fd);
-
-// 			// 	current = current->next;
-// 			// 	// printf("-----\n");
-// 			// }
-
-// 			// printf ("command ==> %s\n", cmds->cmd);
-
-// 			// if (cmds && cmds->cmd && cmds->cmd[0])
-// 			// {
-// 			// 	// if (ft_strcmp(cmds->cmd[0], "echo") == 0 && cmds->cmd[1])
-// 			// 	// {
-// 			// 	// 	write(1, cmds->cmd[1], ft_strlen(cmds->cmd[1]));
-// 			// 	// 	write(1, "\n", 1);
-// 			// 	// 	g_exit_status = 0;
-// 			// 	// }
-// 			// 	// else
-// 			// 	// {
-// 			// 	// 	write(2, "Command not found\n", 18);
-// 			// 	// 	g_exit_status = 127;
-// 			// 	// }
-
-// 			// 	execute_command(cmds, env); //
-// 			// }
-// 			// else
-// 			// 	g_exit_status = 2;
-
-// 			// Free everything
-// 			free(line);
-// 			// free_tokens(tokens);
-// 			// free_commands(cmds);
-// 			gc_free_all(&gc); // clean all GC-tracked memory
-// 		}
-// 	}
-// 	return (0);
-// }
-
-
 int main(int ac, char **av, char **envp)
 {
 	(void)av;
@@ -130,10 +41,10 @@ int main(int ac, char **av, char **envp)
 			if (input && *input)
 			{
 				if (has_pipe)
-					printf("%d\n", has_pipe); // ila kan lpipe at3tik 1 f result
+					printf("ila kayen pipe ==> %d\n", has_pipe); // ila kan lpipe at3tik 1 f result
 				else
 				{
-					printf("%d\n", has_pipe); // daba ila makaynch lpipe at3tik 0 f result o ghat executi bla pipe
+					printf("ila makayenach pipe ==> %d\n", has_pipe); // daba ila makaynch lpipe at3tik 0 f result o ghat executi bla pipe
 					execute_command(cmds, env_list);
 				}
 			}
