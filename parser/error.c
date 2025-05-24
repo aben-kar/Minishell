1fbca831750466086319062c4d1084e551d5bbc4
youzak100@gmail.com <youzak100@gmail.com>
2025-05-24 20:35:04 +0100

    norm

--- parser/error.c
+++ parser/error.c
@@ -4,18 +4,18 @@ void	bash_syntax_error(const char *token)
 {
     if (!token)
     {
-        write(2, "bash: syntax error near unexpected token `newline'\n", 50);
-        return;
+        write (2, "bash: syntax error near unexpected token `newline'\n", 50);
+        return ;
     }
-    write(2, "bash: syntax error near unexpected token `", 42);
-    write(2, token, ft_strlen(token));
-    write(2, "'\n", 2);
+    write (2, "bash: syntax error near unexpected token `", 42);
+    write (2, token, ft_strlen(token));
+    write (2, "'\n", 2);
 }
 
 void	bash_unclosed_quote_error(char quote)
 {
-    write(2, "bash: unexpected EOF while looking for matching `", 48);
-    write(2, &quote, 1);
-    write(2, "'\n", 2);
-    write(2, "bash: syntax error: unexpected end of file\n", 43);
+    write (2, "bash: unexpected EOF while looking for matching `", 48);
+    write (2, &quote, 1);
+    write (2, "'\n", 2);
+    write (2, "bash: syntax error: unexpected end of file\n", 43);
 }
