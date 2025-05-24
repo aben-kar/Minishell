b636b262b1172f97ec3d6099ff06f296b298202a
youzak100@gmail.com <youzak100@gmail.com>
2025-05-24 21:34:32 +0100

    fixed quoted string

--- parser/tokenizer.c
+++ parser/tokenizer.c
@@ -18,7 +18,7 @@ static void	skip_whitespace(const char *input, int *i)
 		(*i)++;
 }
 
-static t_token	*handle_quoted_token(const char *input, int *i, t_token *last, t_token **head, t_gc **gc)
+static t_token *handle_quoted_token(const char *input, int *i, t_token *last, t_token **head, t_gc **gc)
 {
 	int		start;
 	char	quote;
@@ -35,7 +35,7 @@ static t_token	*handle_quoted_token(const char *input, int *i, t_token *last, t_
 		bash_unclosed_quote_error(quote);
 		return (NULL);
 	}
-	val = ft_substr_gc(input, start, *i - start, gc);
+	val = ft_substr_gc(input, start + 1, *i - start - 2, gc);
 	last = add_token(last, val, gc);
 	if (!*head)
 		*head = last;
@@ -59,14 +59,32 @@ static t_token	*handle_operator_token(const char *input, int *i, t_token *last, 
 	return (last);
 }
 
-static t_token	*handle_word_token(const char *input, int *i, t_token *last, t_token **head, t_gc **gc)
+static t_token *handle_word_token(const char *input, int *i, t_token *last, t_token **head, t_gc **gc)
 {
 	int		start;
+	char	quote;
 	char	*val;
 
 	start = *i;
-	while (input[*i] && !ft_isspace(input[*i]) && !is_operator(input[*i]) && input[*i] != '\'' && input[*i] != '"')
-		(*i)++;
+	while (input[*i] && !ft_isspace(input[*i]) && !is_operator(input[*i]))
+	{
+		if (input[*i] == '\'' || input[*i] == '"')
+		{
+			quote = input[*i];
+			(*i)++;
+			while (input[*i] && input[*i] != quote)
+				(*i)++;
+			if (input[*i] == quote)
+				(*i)++;
+			else
+			{
+				bash_unclosed_quote_error(quote);
+				return (NULL);
+			}
+		}
+		else
+			(*i)++;
+	}
 	val = ft_substr_gc(input, start, *i - start, gc);
 	last = add_token(last, val, gc);
 	if (!*head)
