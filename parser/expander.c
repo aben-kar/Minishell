947ce937d838e664d15fbf6244f12dae4e372512
youzak100@gmail.com <youzak100@gmail.com>
2025-05-27 17:15:45 +0100

    tests

--- parser/expander.c
+++ parser/expander.c
@@ -44,35 +44,39 @@ static int	handle_double_quote(const char *word, char **res,
 	while (word[j] && word[j] != '"')
 		j++;
 	inner = ft_strndup(word + i + 1, j - i - 1, gc);
-	*res = ft_strjoin_gc(*res, expand_word(inner, gc, env), gc);
+	*res = ft_strjoin_gc(*res, expand_inside_double_quotes(inner, gc, env), gc);
 	return (j + 1);
 }
 
-static int	handle_dollar(const char *word, char **res,
-	int i, t_gc **gc, t_env *env)
-{
-	char	*key;
-	char	*val;
-	int		j;
-	char	*exit_str;
+// static int	handle_dollar(const char *word, char **res,
+// 	int i, t_gc **gc, t_env *env)
+// {
+// 	char	*key;
+// 	char	*val;
+// 	int		j;
+// 	char	*exit_str;
 
-	if (word[i + 1] == '?')
-	{
-		exit_str = ft_itoa_gc(g_exit_status, gc);
-		*res = ft_strjoin_gc(*res, exit_str, gc);
-		return (i + 2);
-	}
-	j = i + 1;
-	while (ft_isalnum(word[j]) || word[j] == '_')
-		j++;
-	key = ft_strndup(word + i + 1, j - i - 1, gc);
-	val = get_env_val(key, env);
-	if (val != NULL)
-		*res = ft_strjoin_gc(*res, ft_strdup_gc(val, gc), gc);
-	else
-		*res = ft_strjoin_gc(*res, ft_strdup_gc("", gc), gc);
-	return (j);
-}
+// 	if (word[i + 1] == '?')
+// 	{
+// 		exit_str = ft_itoa_gc(g_exit_status, gc);
+// 		*res = ft_strjoin_gc(*res, exit_str, gc);
+// 		return (i + 2);
+// 	}
+// 	if (!word[i + 1] || (!ft_isalpha(word[i + 1]) && word[i + 1] != '_'))
+// 	{
+// 		// Treat invalid variable like "$" or "$)" as literal "$"
+// 		*res = ft_strjoin_char_gc(*res, '$', gc);
+// 		return (i + 1);
+// 	}
+// 	j = i + 1;
+// 	while (ft_isalnum(word[j]) || word[j] == '_')
+// 		j++;
+// 	key = ft_strndup(word + i + 1, j - i - 1, gc);
+// 	val = get_env_val(key, env);
+// 	if (val)
+// 		*res = ft_strjoin_gc(*res, ft_strdup_gc(val, gc), gc);
+// 	return (j);
+// }
 
 char	*expand_word(const char *word, t_gc **gc, t_env *env)
 {
--- parser/parse_utils.c
+++ parser/parse_utils.c
@@ -39,3 +39,51 @@ bool	handle_argument(t_command *cmd, t_token *token,
 	cmd->cmd = argv_add(cmd->cmd, expanded, gc);
 	return (true);
 }
+
+char	*expand_inside_double_quotes(const char *str, t_gc **gc, t_env *env)
+{
+	char	*res = ft_strdup_gc("", gc);
+	int		i = 0;
+
+	while (str[i])
+	{
+		if (str[i] == '$')
+			i = handle_dollar(str, &res, i, gc, env);
+		else
+		{
+			res = ft_strjoin_char_gc(res, str[i], gc);
+			i++;
+		}
+	}
+	return (res);
+}
+
+int	handle_dollar(const char *word, char **res,
+	int i, t_gc **gc, t_env *env)
+{
+	char	*key;
+	char	*val;
+	int		j;
+	char	*exit_str;
+
+	if (word[i + 1] == '?')
+	{
+		exit_str = ft_itoa_gc(g_exit_status, gc);
+		*res = ft_strjoin_gc(*res, exit_str, gc);
+		return (i + 2);
+	}
+	if (!word[i + 1] || (!ft_isalpha(word[i + 1]) && word[i + 1] != '_'))
+	{
+		// Treat invalid variable like "$" or "$)" as literal "$"
+		*res = ft_strjoin_char_gc(*res, '$', gc);
+		return (i + 1);
+	}
+	j = i + 1;
+	while (ft_isalnum(word[j]) || word[j] == '_')
+		j++;
+	key = ft_strndup(word + i + 1, j - i - 1, gc);
+	val = get_env_val(key, env);
+	if (val)
+		*res = ft_strjoin_gc(*res, ft_strdup_gc(val, gc), gc);
+	return (j);
+}
